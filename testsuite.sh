9c844d709ba55b4c7ee4d2319f063c175c5ec1ae
Christophe Lincoln <pankso@slitaz.org>
2012-05-16 16:54:42 +0200

    cosmetic change to testsuite

--- testsuite.sh
+++ testsuite.sh
@@ -2,18 +2,25 @@
 #
 . /lib/libtaz.sh
 
-#boldify "Checking: spk-add"
+newline
+boldify "Checking: spk bc"
+./spk bc
+#boldify "Checking: spk-add bc"
 #./spk-add bc bc --forced
-#boldify "Checking: spk-rm"
+#boldify "Checking: spk-rm bc"
 #./spk-rm bc
 
+# Check libspk.sh functions usage.
 echo -n "$(boldify "Checking: libspk.sh functions")"
 indent 34 "$(colorize $(grep "[a-z]() {" lib/libspk.sh | wc -l) 32)"
-
+separator
 grep "[a-z]() {" lib/libspk.sh | while read line
 do
 	func=`echo "$line" | cut -d '(' -f 1`
 	echo -n "Checking: ${func}()"
 	indent 34 "$(grep "$func" spk* | wc -l)"
 done
+separator
+echo "Run slitaz-base-files testsuite.sh to check libpkg.sh"
+newline
 exit 0
