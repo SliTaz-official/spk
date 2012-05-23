1cc5898f2bb99f78dc5ff46fa6a15873823293c8
Christophe Lincoln <pankso@slitaz.org>
2012-05-23 17:48:23 +0200

    Fix spk-add and small clean

--- lib/libspk.sh
+++ lib/libspk.sh
@@ -191,25 +191,18 @@ extract_package() {
 	local target_dir=$2
 
 	# Validate the file
-	check_valid_tazpkg $package_file
+	#check_valid_tazpkg $package_file
 
 	# Find the package name
 	local package_name=$(package_name $package_file)
 
-	# Create destination directory
+	# Create destination directory and copy package
 	local dest_dir=$(pwd)/$package_name
 	[ -n "$target_dir" ] && dest_dir=$target_dir/$package_name
 	mkdir -p $dest_dir
-
-	newline
-	echo $(boldify $(gettext "Extracting:")) $package_name
-	separator
-
-	gettext "Copying original package..."
 	cp $package_file $dest_dir
-	status
-	
-	cd $dest_dir	
+
+	cd $dest_dir
 	size=$(du -sh $package_file | awk '{print $1}')
 	echo -n $(gettext "Extracting archive"): $size
 	cpio -idm --quiet < ${package_file##*/}
@@ -218,11 +211,6 @@ extract_package() {
 	rm fs.cpio.lzma
 	status
 	cd - > /dev/null
-	
-	separator
-	echo -n "$package_name"
-	gettext "is extracted to:"; echo " $dest_dir"
-	newline
 }
 
 # Unser var set by mirrored_pkg
